4221731e6aac69a9b7c61e00408e469ac82ff3ee
Patrick Bos <egpbos@gmail.com>
2016-10-05 10:50:42 +0200

    Did a callgrind pprofile run. Also cleaned up tensorflow_roofit_demo_2-stripped.py from commented out code.

--- tensorflow_roofit_demo_2-stripped.py
+++ tensorflow_roofit_demo_2-stripped.py
@@ -2,11 +2,10 @@
 # @Author: patrick
 # @Date:   2016-09-01 17:04:53
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-10-05 07:47:51
+# @Last Modified time: 2016-10-05 10:49:55
 
 import tensorflow as tf
 import numpy as np
-# import scipy as sc
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 
@@ -37,9 +36,6 @@ constraint['nsig'] = (0., 10000)
 constraint['nbkg'] = (0., 10000)
 constraint['mes'] = (5.20, 5.30)
 
-# def gaussian_pdf(x, m, s):
-#     return sc.stats.norm.pdf(x, loc=m, scale=s)
-
 pi = tf.constant(np.pi, dtype=tf.float64, name="pi")
 sqrt2pi = tf.constant(np.sqrt(2 * np.pi), dtype=tf.float64, name="sqrt2pi")
 two = tf.constant(2, dtype=tf.float64, name="two")
@@ -55,11 +51,7 @@ def gaussian_pdf(x, mean, std):
 
 def argus_pdf(m, m0, c, p=0.5):
     t = m / m0
-    # if (t >= 1):
     u = 1 - t * t
-    # return tf.select(tf.greater_equal(t, one),
-    #                  zero,
-    #                  m * tf.pow(u, p) * tf.exp(c * u))
     return tf.cond(tf.greater_equal(t, one),
                    lambda: zero,
                    lambda: m * tf.pow(u, p) * tf.exp(c * u), name="argus_pdf")
@@ -68,27 +60,7 @@ def argus_pdf(m, m0, c, p=0.5):
     #       Dat is dus bij select niet mogelijk, daar krijg je meteen beide hele
     #       tensors.
 
-    # u = 1 - t * t
-    # return m * tf.pow(1 - t * t, p) * tf.exp(c * (1 - t * t))
 
-
-# Double_t RooArgusBG::analyticalIntegral(Int_t code, const char* rangeName) const
-# {
-#   R__ASSERT(code==1);
-#   // Formula for integration over m when p=0.5
-#   static const Double_t pi = atan2(0.0,-1.0);
-#   Double_t min = (m.min(rangeName) < m0) ? m.min(rangeName) : m0;
-#   Double_t max = (m.max(rangeName) < m0) ? m.max(rangeName) : m0;
-#   Double_t f1 = (1.-TMath::Power(min/m0,2));
-#   Double_t f2 = (1.-TMath::Power(max/m0,2));
-#   Double_t aLow, aHigh ;
-#   aLow  = -0.5*m0*m0*(exp(c*f1)*sqrt(f1)/c + 0.5/TMath::Power(-c,1.5)*sqrt(pi)*RooMath::erf(sqrt(-c*f1)));
-#   aHigh = -0.5*m0*m0*(exp(c*f2)*sqrt(f2)/c + 0.5/TMath::Power(-c,1.5)*sqrt(pi)*RooMath::erf(sqrt(-c*f2)));
-#   Double_t area = aHigh - aLow;
-#   //cout << "c = " << c << "aHigh = " << aHigh << " aLow = " << aLow << " area = " << area << endl ;
-#   return area;
-
-# }
 def argus_integral_phalf(m_low, m_high, m0, c):
     """
     Only valid for argus_pdf with p=0.5! Otherwise need to do numerical
@@ -147,47 +119,33 @@ def argus_pdf_phalf_WN(m, m0, c, m_low, m_high, tf_norm=tf.constant(False)):
 # // --- Observable ---
 # RooRealVar mes("mes","m_{ES} (GeV)",5.20,5.30) ;
 
-# N.B.: tf heeft geen bounds in Variable!
-# mes = tf.Variable("mes", 5.25)
-
 # // --- Build Gaussian signal PDF ---
 # RooRealVar sigmean("sigmean","B^{#pm} mass",5.28,5.20,5.30) ;
 # RooRealVar sigwidth("sigwidth","B^{#pm} width",0.0027,0.001,1.) ;
 
 sigmean = tf.Variable(np.float64(5.28), name="sigmean")
-# sigmean_c = tf.clip_by_value(sigmean, 5.20, 5.30)
 sigwidth = tf.Variable(np.float64(0.0027), name="sigwidth")
-# sigwidth_c = tf.clip_by_value(sigwidth, 0.001, 1.)
 
 # RooGaussian gauss("gauss","gaussian PDF",mes,sigmean,sigwidth) ;
-
-# gauss = lambda mes: gaussian_pdf(mes, sigmean, sigwidth)
 
 # // --- Build Argus background PDF ---
 # RooRealVar argpar("argpar","argus shape parameter",-20.0,-100.,-1.) ;
 # RooConstVar m0("m0", "resonant mass", 5.291);
 
 argpar = tf.Variable(np.float64(argpar_num), name="argpar")
-# argpar_c = tf.clip_by_value(argpar, -100., -1.)
 m0 = tf.constant(np.float64(m0_num), name="m0")
 
 # RooArgusBG argus("argus","Argus PDF",mes,m0,argpar) ;
-
-# argus = lambda mes: argus_pdf(mes, m0, argpar)
 
 # // --- Construct signal+background PDF ---
 # RooRealVar nsig("nsig","#signal events",200,0.,10000) ;
 # RooRealVar nbkg("nbkg","#background events",800,0.,10000) ;
 
 nsig = tf.Variable(np.float64(200), name="nsig")
-# nsig_c = tf.clip_by_value(nsig, 0., 10000)
 nbkg = tf.Variable(np.float64(800), name="nbkg")
-# nbkg_c = tf.clip_by_value(nbkg, 0., 10000)
 
 
 # RooAddPdf sum("sum","g+a",RooArgList(gauss,argus),RooArgList(nsig,nbkg)) ;
-
-# sum_pdf = lambda mes: nsig * gauss(mes) + nbkg * argus(mes)
 
 # // --- Generate a toyMC sample from composite PDF ---
 # RooDataSet *data = sum.generate(mes,2000) ;
@@ -202,16 +160,7 @@ def sum_pdf_test(mes, nsig, sigmean, sigwidth, nbkg, m0, argpar, mes_low, mes_hi
 
 sum_pdf_vec = np.vectorize(sum_pdf, otypes=[np.float])
 
-
-
-# def sum_pdf_mes(mes):
-#     # mes_c = tf.clip_by_value(mes, 5.20, 5.30)
-#     # mes_c = apply_constraint(mes, constraint)
-#     # return nsig_c * gaussian_pdf(mes, sigmean_c, sigwidth_c) + nbkg_c * argus_pdf(mes, m0, argpar_c)
-#     return nsig * gaussian_pdf(mes, sigmean, sigwidth) + nbkg * argus_pdf(mes, m0, argpar)
-
-
-# ok, dit is hier niet triviaal, dus gewoon in RooFit genereren en importeren
+# data in RooFit genereren en importeren
 # draai dit in ROOT:
 # data.write("roofit_demo_random_data_values.dat");
 data_raw = np.loadtxt(project_dn + "roofit_demo_random_data_values.dat",
@@ -228,24 +177,8 @@ for key in constraint.keys():
     constraint[key] = (tf.constant(low, dtype=tf.float64),
                        tf.constant(high, dtype=tf.float64))
 
-# using:
-# https://www.tensorflow.org/versions/r0.10/api_docs/python/train.html#optimizers
-# https://gist.github.com/ibab/45c3d886c182a1ea26d5
-# http://stackoverflow.com/a/36267185/1199693
-
 nll = tf.neg(tf.reduce_sum(tf.log(tf.map_fn(lambda mes: sum_pdf(mes, nsig, sigmean, sigwidth, nbkg, m0, argpar, constraint['mes'][0], constraint['mes'][1]), data))), name="nll")
 # grad = tf.gradients(nll, [mu, sigma])
-
-
-# def objective(params):
-#     mu_, sigma_ = params
-#     return sess.run(nll, feed_dict={mes: data})
-
-
-# def gradient(params):
-#     mu_, sigma_ = params
-#     ret =  sess.run(grad, feed_dict={ mu: mu_, sigma: sigma_ })
-#     return np.array(ret)
 
 max_steps = 10
 
@@ -263,7 +196,6 @@ variables = tf.all_variables()
 # opt = tf.train.GradientDescentOptimizer(learning_rate=0.001)
 # opt = tf.train.AdagradOptimizer(learning_rate=0.1)
 opt = tf.train.AdamOptimizer()
-# opt_op = opt.minimize(nll, var_list=[sigmean, sigwidth, argpar, nsig, nbkg])
 opt_op = opt.minimize(nll)
 
 tf.scalar_summary('nll', nll)
@@ -306,21 +238,6 @@ with tf.Session() as sess:
         print "opt\t" + "\t".join(["%6.4e" % v for v in var_values_opt]) + "\t | %f" % nll_value_opt
         # clipped = np.where(var_values_opt == var_values_clip, [" "*10] * len(variables), ["%6.4e" % v for v in var_values_clip])
         # print "clip\t" + "\t".join(clipped) + "\t | %f" % nll_value_clip
-        # Compute the gradients for a list of variables.
-        # grads_and_vars = opt.compute_gradients(nll, [sigmean, sigwidth, argpar, nsig, nbkg])
-        # print grads_and_vars
-
-        # for gv in grads_and_vars:
-        #     apply_constraint(gv[1], constraint)
-
-        # grads_and_vars is a list of tuples (gradient, variable).  Do whatever you
-        # need to the 'gradient' part, for example cap them, etc.
-        # capped_grads_and_vars = [(gv[0], apply_constraint(gv[1], constraint)) for gv in grads_and_vars]
-
-        # Ask the optimizer to apply the capped gradients.
-        # out = opt.apply_gradients(capped_grads_and_vars)
-        # out = opt.apply_gradients(grads_and_vars)
-        # print sess.run([out, nll, sigmean, sigwidth, argpar, nsig, nbkg])
 
     end = timer()
 
