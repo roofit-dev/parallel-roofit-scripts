7f9c7ea375398e53268ac769a32c11e8350a134b
Patrick Bos <egpbos@gmail.com>
2016-10-12 14:18:53 +0200

    Clip argument instead of sqrt...

--- tensorflow_roofit_demo_5_sqrt_grad_nan_simple.py
+++ tensorflow_roofit_demo_5_sqrt_grad_nan_simple.py
@@ -2,7 +2,7 @@
 # @Author: patrick
 # @Date:   2016-09-01 17:04:53
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-10-12 14:17:24
+# @Last Modified time: 2016-10-12 14:18:48
 
 # as per tensorflow styleguide
 # https://www.tensorflow.org/versions/r0.11/how_tos/style_guide.html
@@ -21,6 +21,7 @@ sqrt_grad = tf.gradients(tf.sqrt(c), c, name="sqrt_c_GRAD")
 selsqrt_grad = tf.gradients(tf.select(c > 0, tf.sqrt(c), 0), c, name="selsqrt_c_GRAD")
 
 clipsqrt_grad = tf.gradients(tf.clip_by_value(tf.sqrt(c), 1e-10, 1), c, name="clipsqrt_c_GRAD")
+clipargsqrt_grad = tf.gradients(tf.sqrt(tf.clip_by_value(c, 1e-10, 1)), c, name="clipargsqrt_c_GRAD")
 
 init_op = tf.initialize_all_variables()
 
@@ -32,4 +33,5 @@ with tf.Session() as sess:
     sqrt_grad_value = sess.run(sqrt_grad)
     selsqrt_grad_value = sess.run(selsqrt_grad)
     clipsqrt_grad_value = sess.run(clipsqrt_grad)
-    print(sqrt_grad_value, selsqrt_grad_value, clipsqrt_grad_value)
+    clipargsqrt_grad_value = sess.run(clipargsqrt_grad)
+    print(sqrt_grad_value, selsqrt_grad_value, clipsqrt_grad_value, clipargsqrt_grad_value)
