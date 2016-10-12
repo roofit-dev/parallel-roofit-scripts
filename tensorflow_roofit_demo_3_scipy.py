37d97e4ad9e6e39f3e4cfd6220507abee63f66c0
Patrick Bos <egpbos@gmail.com>
2016-10-12 10:36:47 +0200

    Added name scope for easier debugging in TBoard

--- tensorflow_roofit_demo_3_scipy.py
+++ tensorflow_roofit_demo_3_scipy.py
@@ -2,7 +2,7 @@
 # @Author: patrick
 # @Date:   2016-09-01 17:04:53
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-10-12 09:47:47
+# @Last Modified time: 2016-10-12 10:36:26
 
 # as per tensorflow styleguide
 # https://www.tensorflow.org/versions/r0.11/how_tos/style_guide.html
@@ -95,12 +95,13 @@ def argus_integral_phalf(m_low, m_high, m0, c):
     Only valid for argus_pdf with p=0.5! Otherwise need to do numerical
     integral.
     """
-    def F(x):
-        # return -0.5 * m0 * m0 * (tf.exp(c * x) * tf.sqrt(x) / c + 0.5 / tf.pow(-c, 1.5) * tf.sqrt(pi) * tf.erf(tf.sqrt(-c * x)))
-        # fragiel_ding = tf.sqrt(-c * x, name="fragiel_ding")
-        fragiel_ding = tf.pow(-c * x, 1, name="fragiel_ding")
-        return -0.5 * m0 * m0 * (tf.exp(c * x) * tf.sqrt(x) / c + 0.5 / tf.pow(-c, 1.5) * tf.sqrt(pi)# * tf.erf(tf.sqrt(-c * x)))
-                                 * tf.erf(fragiel_ding))
+    def F(x, name=None):
+        with tf.name_scope(name, "argus_integral_phalf_primitive"):
+            # return -0.5 * m0 * m0 * (tf.exp(c * x) * tf.sqrt(x) / c + 0.5 / tf.pow(-c, 1.5) * tf.sqrt(pi) * tf.erf(tf.sqrt(-c * x)))
+            fragiel_ding = tf.sqrt(-c * x, name="fragiel_ding")
+            # fragiel_ding = tf.pow(-c * x, 0.5, name="fragiel_ding")
+            return -0.5 * m0 * m0 * (tf.exp(c * x) * tf.sqrt(x) / c + 0.5 / tf.pow(-c, 1.5) * tf.sqrt(pi)# * tf.erf(tf.sqrt(-c * x)))
+                                     * tf.erf(fragiel_ding))
 
     a = tf.minimum(m_low, m0)
     b = tf.minimum(m_high, m0)
@@ -108,7 +109,7 @@ def argus_integral_phalf(m_low, m_high, m0, c):
     x1 = 1 - tf.pow(a / m0, 2)
     x2 = 1 - tf.pow(b / m0, 2)
 
-    area = tf.sub(F(x2), F(x1), name="argus_integral_phalf")
+    area = tf.sub(F(x2, name="F2"), F(x1, name="F1"), name="argus_integral_phalf")
     return area
 
 
