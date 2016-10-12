391102754c8382009580d629566a8fb8bdfa2913
Patrick Bos <egpbos@gmail.com>
2016-10-12 14:26:08 +0200

    Added some variants to minimal example

--- tensorflow_roofit_demo_5_sqrt_grad_nan_simple.py
+++ tensorflow_roofit_demo_5_sqrt_grad_nan_simple.py
@@ -2,7 +2,7 @@
 # @Author: patrick
 # @Date:   2016-09-01 17:04:53
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-10-12 14:18:48
+# @Last Modified time: 2016-10-12 14:25:52
 
 # as per tensorflow styleguide
 # https://www.tensorflow.org/versions/r0.11/how_tos/style_guide.html
@@ -17,21 +17,25 @@ c = tf.Variable(0.0, name="c")
 
 sqrt_grad = tf.gradients(tf.sqrt(c), c, name="sqrt_c_GRAD")
 
+# another possibility is when another factor in the argument is zero
+x = tf.Variable(1., name="x")
+sqrt_x_grad = tf.gradients(tf.sqrt(x * c), x, name="sqrt_x_GRAD")
+
 # try to use select to filter out the NaN
 selsqrt_grad = tf.gradients(tf.select(c > 0, tf.sqrt(c), 0), c, name="selsqrt_c_GRAD")
 
+# try clipping of the sqrt
 clipsqrt_grad = tf.gradients(tf.clip_by_value(tf.sqrt(c), 1e-10, 1), c, name="clipsqrt_c_GRAD")
+
+# clip the argument of the sqrt --> only numerically stable option
 clipargsqrt_grad = tf.gradients(tf.sqrt(tf.clip_by_value(c, 1e-10, 1)), c, name="clipargsqrt_c_GRAD")
 
 init_op = tf.initialize_all_variables()
-
 
 with tf.Session() as sess:
     summary_writer = tf.train.SummaryWriter('./train_5_%i' % int(time.time()), sess.graph)
     sess.run(init_op)
 
-    sqrt_grad_value = sess.run(sqrt_grad)
-    selsqrt_grad_value = sess.run(selsqrt_grad)
-    clipsqrt_grad_value = sess.run(clipsqrt_grad)
-    clipargsqrt_grad_value = sess.run(clipargsqrt_grad)
-    print(sqrt_grad_value, selsqrt_grad_value, clipsqrt_grad_value, clipargsqrt_grad_value)
+    print(sess.run([sqrt_grad, sqrt_x_grad, selsqrt_grad,
+                    clipsqrt_grad, clipargsqrt_grad]))
+    # [[inf], [nan], [nan], [nan], [0.0]]
