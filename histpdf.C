104392f202461eb31eb06ed64f21f9f4b87954eb
Patrick Bos <egpbos@gmail.com>
2016-10-17 10:40:09 +0200

    Added histpdf.C

---
+++ histpdf.C
@@ -0,0 +1,27 @@
+void histpdf()
+{
+  
+  // Make workspace met parent distributions
+  RooWorkspace w("w",1) ;
+  w.factory("Gaussian::g(x[-10,10],0,3)") ;
+  w.factory("Uniform::u(x)") ;
+
+  // Sample twee histogrammen van parent distributions
+  RooDataHist* h_g = w.pdf("g")->generateBinned(*w.var("x"),1000) ;
+  RooDataHist* h_u = w.pdf("u")->generateBinned(*w.var("x"),1000) ;
+
+  // Make 2 pdfs die histogram also onderliggende implementatie hebben
+  RooHistPdf hp_g("hp_g","hp_g",*w.var("x"),*h_g) ;
+  RooHistPdf hp_u("hp_u","hp_u",*w.var("x"),*h_u) ;
+
+  // Construct pdf als some van twee histogram-based pdf
+  RooRealVar frac("frac","frac",0,1) ;
+  RooAddPdf model("model","model",RooArgList(hp_g,hp_u),frac) ;
+
+  // Sample toy data van gezamenlijk model
+  RooDataHist* toyData = model.generateBinned(*w.var("x"),1000) ;
+  
+  // Fit toy daya
+  model.fitTo(*toyData) ;
+  
+}
