1cb8d5c23ed199ee990e4047cbe86912a03242a5
Patrick Bos <egpbos@gmail.com>
2016-10-19 11:01:39 +0200

    expanduser for path here as well

--- tensorflow_histpdf.py
+++ tensorflow_histpdf.py
@@ -2,7 +2,7 @@
 # @Author: Patrick Bos
 # @Date:   2016-10-17 18:12:26
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-10-19 10:06:12
+# @Last Modified time: 2016-10-19 11:01:32
 
 # as per tensorflow styleguide
 # https://www.tensorflow.org/versions/r0.11/how_tos/style_guide.html
@@ -15,14 +15,14 @@ from tensorflow.python.platform import tf_logging as logging
 import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+import os
 
 from histpdf_data import combined_raw, gaussian_raw, uniform_raw
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-project_dn = "/home/patrick/projects/apcocsm/"
-# project_dn = "/home/pbos/apcocsm/"
+project_dn = os.path.expanduser("~/projects/apcocsm/")
 
 N = eventWeight = tf.constant([i['weight'] for i in combined_raw], dtype=tf.float64)
 
