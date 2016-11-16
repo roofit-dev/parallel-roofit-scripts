644c4a6045277216dd98e7920e8a3b822df62881
Patrick Bos <egpbos@gmail.com>
2016-11-16 14:29:43 +0100

    Removed linter warnings and added command line input

--- scaling/unbinned_scaling.cpp
+++ scaling/unbinned_scaling.cpp
@@ -1,30 +1,47 @@
 #include <chrono>
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <string>
 
 using namespace RooFit;
 
-void unbinned_scaling() {
-  int N_gaussians(20);
-  int N_observables(5);
-  int N_parameters(12);  // must be even, means and sigmas have diff ranges
-  int N_events(1000);
+// call from command line like, for instance:
+// root -l 'unbinned_scaling.cpp(20,5,8,1000)'
 
+void unbinned_scaling(int N_gaussians, int N_observables, int N_parameters,
+                      int N_events) {
+  // scaling configuration
+  // int N_gaussians(20);
+  // int N_observables(5);
+  // int N_parameters(8);  // must be even, means and sigmas have diff ranges
+  // int N_events(1000);
+
+  // plotting configuration
   int obs_plot_x(3);
   int obs_plot_y(2);
   int obs_plot_x_px(1200);
   int obs_plot_y_px(800);
 
-  // int N_timing_loops(1);
-  // int printlevel(3);
+  // other stuff
+  int printlevel(0);
   int optimizeConst(2);
+  // int N_timing_loops(1); // not used
 
+  // some sanity checks
+  if (obs_plot_x * obs_plot_y < N_observables) {
+    std::cout << "WARNING: obs_plot_x * obs_plot_y < N_observables,"
+                 " won't be able to plot all observables!"
+              << std::endl << std::endl;
+  }
   if (N_parameters % 2 != 0) {
     std::cout << "set N_parameters to an even number!" << std::endl;
     exit(2);
   }
 
-  RooWorkspace w("w",1) ;
+  // here we go!
+
+  RooWorkspace w("w", 1) ;
 
   RooArgSet obs_set;
 
@@ -41,9 +58,10 @@ void unbinned_scaling() {
     std::ostringstream os;
     // int ix_p = (ix/2) % N_parameters;
     int ix_p = ix % (N_parameters / 2);
-    os << "Gaussian::g" << ix << "(x" << ix % N_observables << "[-10,10],"
-                              << "m" << ix_p << "[" << mean[ix_p] << ",-10,10],"
-                              << "s" << ix_p << "[" << sigma[ix_p] << ",0.1,10])";
+    os << "Gaussian::g" << ix
+       << "(x" << ix % N_observables << "[-10,10],"
+       << "m" << ix_p << "[" << mean[ix_p] << ",-10,10],"
+       << "s" << ix_p << "[" << sigma[ix_p] << ",0.1,10])";
     std::string s = os.str();
     w.factory(s.c_str());
   }
@@ -54,7 +72,7 @@ void unbinned_scaling() {
       std::ostringstream os;
       os << "Uniform::u" << ix << "(x" << ix << ")";
       std::string s = os.str();
-      w.factory(s.c_str()); 
+      w.factory(s.c_str());
     }
 
     // gather the observables in a list for data generation below
@@ -83,7 +101,7 @@ void unbinned_scaling() {
   }
   // gather them in count_set
   for (int ix = 0; ix < N_gaussians; ++ix) {
-    std::stringstream os, os2;
+    std::stringstream os;
     os << "Nsig" << ix;
     std::string s = os.str();
     count_set.add(*w.arg(s.c_str()));
@@ -100,7 +118,7 @@ void unbinned_scaling() {
   }
   // gather them in count_set
   for (int ix = 0; ix < N_observables; ++ix) {
-    std::stringstream os, os2;
+    std::stringstream os;
     os << "Nbkg" << ix;
     std::string s = os.str();
     count_set.add(*w.arg(s.c_str()));
@@ -123,39 +141,45 @@ void unbinned_scaling() {
   // sum.fitTo(*data,"Extended") ;
   // instead of full fitTo, only do the fit, leave out error matrix, using
   // run style of run_higgs.C
-  RooAbsReal* nll = sum.createNLL(*data,"Extended");
-  RooMinimizer m(*nll) ;
-  // m.setVerbose(1) ;
-  m.setStrategy(0) ;
-  m.setProfile(1) ;
-  m.optimizeConst(optimizeConst) ;
+  RooAbsReal* nll = sum.createNLL(*data, "Extended");
+  RooMinimizer m(*nll);
+  // m.setVerbose(1);
+  m.setStrategy(0);
+  m.setProfile(1);
+  m.setPrintLevel(printlevel);
+  m.optimizeConst(optimizeConst);
 
   auto begin = std::chrono::high_resolution_clock::now();
   // m.hesse();
 
-  m.minimize("Minuit2","migrad") ;
+  m.minimize("Minuit2", "migrad");
 
   auto end = std::chrono::high_resolution_clock::now();
-  std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() / 1e9  << "s" << std::endl;
+
+  float timing_ns = std::chrono::duration_cast<std::chrono::nanoseconds>
+                    (end-begin).count();
+  std::cout << timing_ns / 1e9  << "s" << std::endl;
 
   // print the "true" values for comparison
-  std::cout << "--- values of PDF parameters used for data generation:" << std::endl;
+  std::cout << "--- values of PDF parameters used for data generation:"
+            << std::endl;
   for (int ix = 0; ix < N_parameters/2; ++ix) {
-    std::cout << "    gauss " << ix << ": m = " << mean[ix] << ", s = " << sigma[ix] << std::endl;
+    std::cout << "    gauss " << ix << ": m = " << mean[ix] << ", s = "
+              << sigma[ix] << std::endl;
   }
 
   // --- Plot toy data and composite PDF overlaid ---
-  TCanvas* c = new TCanvas("unbinned_scaling","unbinned_scaling",obs_plot_x_px,obs_plot_y_px) ;
-  c->Divide(obs_plot_x, obs_plot_y) ;
-  // c->cd(1) ; gPad->SetLeftMargin(0.15) ; x0frame->GetYaxis()->SetTitleOffset(1.4) ; x0frame->Draw() ;
-  // c->cd(2) ; gPad->SetLeftMargin(0.15) ; x1frame->GetYaxis()->SetTitleOffset(1.4) ; x1frame->Draw() ;
+  TCanvas* c = new TCanvas("unbinned_scaling", "unbinned_scaling",
+                           obs_plot_x_px, obs_plot_y_px);
+  c->Divide(obs_plot_x, obs_plot_y);
   for (int ix = 0; ix < N_observables && ix < obs_plot_x * obs_plot_y; ++ix) {
     std::ostringstream os;
     os << "x" << ix;
     std::string s = os.str();
-    RooPlot* frame = w.var(s.c_str())->frame() ;
-    data->plotOn(frame) ;
-    sum.plotOn(frame) ;
-    c->cd(ix+1) ; frame->Draw() ;
+    RooPlot* frame = w.var(s.c_str())->frame();
+    data->plotOn(frame);
+    sum.plotOn(frame);
+    c->cd(ix+1);
+    frame->Draw();
   }
 }
