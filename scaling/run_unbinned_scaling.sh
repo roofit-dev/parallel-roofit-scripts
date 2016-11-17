677eac6e70f44d6045552ba3a85ebc001b69e370
Patrick Bos <me@nikhef>
2016-11-17 13:39:46 +0100

    Really fixed run script now.

--- scaling/run_unbinned_scaling.sh
+++ scaling/run_unbinned_scaling.sh
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # @Author: Patrick Bos
 # @Date:   2016-11-16 16:54:41
 # @Last Modified by:   Patrick Bos
 # @Last Modified time: 2016-11-16 17:00:06
 
-$HOME/root6apcocsm.sh
+#set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+# DON'T USE SET -E TOGETHER WITH EXPAND_ALIASES!
+shopt -s expand_aliases
+# ALIASES MOETEN AANGEZET WORDEN (http://unix.stackexchange.com/a/1498/193258)
+
+source $HOME/root6apcocsm.sh
+
+# go to run-dir
+cd $HOME/project_atlas/apcocsm_code/scaling
 
 for g in 20; do
 for o in 1; do  # don't modify observables!
@@ -17,3 +26,5 @@ for seed in 1 1 2 2 3 3 4 4 5 5; do
 root -b -q -l "unbinned_scaling.cpp(${g},${o},${p},${e},${cpu},${ileave},${seed})"
 
 done; done; done; done; done; done; done
+
+cd -
