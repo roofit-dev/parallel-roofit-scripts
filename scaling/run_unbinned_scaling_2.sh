caf3689b527114e3aaa997b0d347541f32e1db2c
Patrick Bos <egpbos@gmail.com>
2016-11-24 07:47:25 +0100

    Updated root source script

--- scaling/run_unbinned_scaling_2.sh
+++ scaling/run_unbinned_scaling_2.sh
@@ -2,7 +2,7 @@
 # @Author: Patrick Bos
 # @Date:   2016-11-16 16:54:41
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-11-21 17:43:22
+# @Last Modified time: 2016-11-24 07:46:27
 
 #set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
@@ -10,7 +10,7 @@
 shopt -s expand_aliases
 # ALIASES MOETEN AANGEZET WORDEN (http://unix.stackexchange.com/a/1498/193258)
 
-source $HOME/root6apcocsm.sh
+source $HOME/root_apcocsm_latest.sh
 
 # go to run-dir
 cd $HOME/project_atlas/apcocsm_code/scaling
