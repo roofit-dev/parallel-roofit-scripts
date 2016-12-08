1f31f2cafef4b7106c4bb3f41e94562c761bdec8
Patrick Bos <me@nikhef>
2016-12-08 10:37:43 +0100

    Bit smarter running of script.

--- scaling/run_unbinned_scaling_2.sh
+++ scaling/run_unbinned_scaling_2.sh
@@ -3,6 +3,7 @@
 # @Date:   2016-11-16 16:54:41
 # @Last Modified by:   Patrick Bos
 # @Last Modified time: 2016-11-24 07:46:27
+# PBS -l nodes=1:ppn=8
 
 #set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
@@ -13,7 +14,8 @@ shopt -s expand_aliases
 source $HOME/root_apcocsm_latest.sh
 
 # go to run-dir
-cd $HOME/project_atlas/apcocsm_code/scaling
+mkdir $HOME/project_atlas/apcocsm_code/scaling/$PBS_JOBID
+cd $HOME/project_atlas/apcocsm_code/scaling/$PBS_JOBID
 
 g=6
 o=1
