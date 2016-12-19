5a73abf1948526ffa0da5f865a016b235539aeae
Patrick Bos <egpbos@gmail.com>
2016-12-19 10:41:10 +0100

    Added new unbinned_scaling runner, including timing_flag

---
+++ scaling/run_unbinned_scaling_3.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# @Author: Patrick Bos
+# @Date:   2016-11-16 16:54:41
+# @Last Modified by:   Patrick Bos
+# @Last Modified time: 2016-12-19 10:40:20
+# PBS -l nodes=1:ppn=8
+
+#set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+# DON'T USE SET -E TOGETHER WITH EXPAND_ALIASES!
+shopt -s expand_aliases
+# ALIASES MOETEN AANGEZET WORDEN (http://unix.stackexchange.com/a/1498/193258)
+
+source $HOME/root_apcocsm_latest.sh
+
+# go to run-dir
+mkdir $HOME/project_atlas/apcocsm_code/scaling/$PBS_JOBID
+cd $HOME/project_atlas/apcocsm_code/scaling/$PBS_JOBID
+
+g=6
+o=1
+p=4
+ileave=0
+seed=1
+
+printlevel=0
+
+for e in 100000 1000000 10000000; do
+for cpu in {1..8}; do
+for timing_flag in {1..7}; do
+
+# do it three times and only use the minimum runtime
+for repeat_nr in {1..3}; do
+
+echo "Repeat number $repeat_nr"
+root -b -q -l "../unbinned_scaling.cpp(${g},${o},${p},${e},${cpu},${ileave},${seed},${printlevel},${timing_flag})"
+
+done; done; done; done
+
+cd -
