03205881531faf746550f147a53293d418eb3f15
Vince Croft <vincecr0ft@users.noreply.github.com>
2017-09-26 16:58:15 +0200

    Adding GaussMinimization test script

---
+++ GaussMinimizer.py
@@ -0,0 +1,22 @@
+import ROOT
+
+w = ROOT.RooWorkspace()
+w.factory('Gaussian::g(x[-5,5],mu[-3,3],sigma[1])')
+x = w.var('x')
+pdf = w.pdf('g')
+data = pdf.generate(ROOT.RooArgSet(x),10000)
+mu = w.var('mu')
+nll = pdf.createNLL(data)
+print "trying nominal calculation"
+m0 = ROOT.RooMinimizer(nll)
+m0.migrad()
+m0.hesse()
+m0.minos()
+print "nominal mu fit is,",mu.getVal()
+
+mu.setVal(-2.9)
+print 'trying GaussMinimizer'
+m1  = ROOT.RooGaussMinimizer(nll)
+m1.migrad()
+print mu.getVal()
+print "Made and minimised an nll with 2 Gradient Function"
