4a616e8c664c8eed800cb68f8fce9fdb39c1c81b
E. G. Patrick Bos <egpbos@gmail.com>
2017-10-25 14:43:10 +0200

    Added debugging output to find out why they differ

--- GradMinimizer.cpp
+++ GradMinimizer.cpp
@@ -3,9 +3,10 @@
 // call from command line like, for instance:
 // root -l 'GradMinimizer.cpp()'
 
-// R__LOAD_LIBRARY(libRooFit)
+R__LOAD_LIBRARY(libRooFit)
 
 #include <iostream>
+// #include <exception>
 
 using namespace RooFit;
 
@@ -19,37 +20,68 @@ void GradMinimizer() {
   RooRealVar * mu = w.var("mu");
 
   RooDataSet * data = pdf->generate(RooArgSet(*x), 10000);
+  mu->setVal(-2.9);
 
   auto nll = pdf->createNLL(*data);
 
-  // set mu to same value at the start of all minimizations
-  mu->setVal(-2.9);
+  // save initial values for the start of all minimizations
+  RooArgSet values = RooArgSet(*mu, *pdf, *nll);
+  
+  std::cout << std::endl << std::endl;
+  values.Print("v");
+  mu->Print("v");
+  std::cout << std::endl << std::endl;
+
+  RooArgSet* savedValues = dynamic_cast<RooArgSet*>(values.snapshot());
+  if (savedValues == nullptr) {
+    throw std::runtime_error("params->snapshot() cannot be casted to RooArgSet!");
+  }
 
   std::cout << "trying nominal calculation" << std::endl;
   RooMinimizer m0(*nll);
+  m0.setPrintLevel(3);
+  m0.setVerbose();
 
   m0.migrad();
 
-  m0.hesse();
+  std::cout << std::endl << std::endl;
+  values.Print("v");
+  mu->Print("v");
+  std::cout << std::endl << std::endl;
 
-  m0.minos();
+  // m0.hesse();
+
+  // m0.minos();
 
   std::cout << "nominal mu fit is," << mu->getVal() << std::endl;
 
-  // set mu to same value at the start of both minimizations
-  mu->setVal(-2.9);
+  // reset initial values
+  std::cout << std::endl << std::endl;
+  values.Print("v");
+  mu->Print("v");
+  std::cout << "\n === reset initial values === \n" << std::endl;
+  values = *savedValues;
+  values.Print("v");
+  mu->Print("v");
+  std::cout << std::endl << std::endl;
 
   std::cout << "trying GradMinimizer" << std::endl;
   RooGradMinimizer m1(*nll);
+  m1.setVerbose();
 
   std::cout << "RooGradMinimizer created" << std::endl;
 
   m1.migrad();
-  std::cout << "migrad worked with mu of " << mu->getVal() << std::endl;
     
   // m1.hesse();
   // std::cout << "hesse done" << std::endl;
   
   // m1.minos();
   // std::cout << "minos done" << std::endl;
+
+  std::cout << std::endl << std::endl;
+  values.Print("v");
+  mu->Print("v");
+  std::cout << std::endl << std::endl;
+
 }
