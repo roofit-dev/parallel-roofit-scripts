27b20e66f27491211f57cca99e7d0dbd5dac162b
E. G. Patrick Bos <egpbos@gmail.com>
2017-10-30 16:34:06 +0100

    GradMinimizer: clean-up, timing

--- GradMinimizer.cpp
+++ GradMinimizer.cpp
@@ -30,57 +30,59 @@ void GradMinimizer() {
   // save initial values for the start of all minimizations
   RooArgSet values = RooArgSet(*mu, *pdf, *nll);
   
-  // std::cout << std::endl << std::endl;
-  // values.Print("v");
-  // mu->Print("v");
-  // std::cout << std::endl << std::endl;
-
   RooArgSet* savedValues = dynamic_cast<RooArgSet*>(values.snapshot());
   if (savedValues == nullptr) {
     throw std::runtime_error("params->snapshot() cannot be casted to RooArgSet!");
   }
 
+  // --------
+
+  RooWallTimer wtimer;
+  // RooCPUTimer ctimer 
+
+  // --------
+
   std::cout << "trying nominal calculation" << std::endl;
+
   RooMinimizer m0(*nll);
   m0.setMinimizerType("Minuit2");
+
   m0.setStrategy(0);
-
-  m0.setPrintLevel(-1);
   // m0.setVerbose();
+  m0.setPrintLevel(0);
 
+  wtimer.start();
   m0.migrad();
+  wtimer.stop();
 
-  // std::cout << std::endl << std::endl;
-  // values.Print("v");
-  // mu->Print("v");
-  // std::cout << std::endl << std::endl;
+  std::cout << "  -- nominal calculation wall clock time:        " << wtimer.timing_s() << "s" << std::endl;
 
   // m0.hesse();
 
   // m0.minos();
 
-  std::cout << "nominal mu fit is " << mu->getVal() << std::endl;
 
-  // reset initial values
-  // std::cout << std::endl << std::endl;
-  // values.Print("v");
-  // mu->Print("v");
+  // --------
+
   std::cout << "\n === reset initial values === \n" << std::endl;
   values = *savedValues;
-  // values.Print("v");
-  // mu->Print("v");
-  // std::cout << std::endl << std::endl;
+
+  // --------
+
 
   std::cout << "trying GradMinimizer" << std::endl;
+
   RooGradMinimizer m1(*nll);
+
   m1.setStrategy(0);
   // m1.setVerbose();
+  m1.setPrintLevel(0);
 
-  // std::cout << "RooGradMinimizer created" << std::endl;
-
+  wtimer.start();
   m1.migrad();
+  wtimer.stop();
 
-  std::cout << "GradMinimizer mu fit is " << mu->getVal() << std::endl;
+  std::cout << "  -- GradMinimizer calculation wall clock time:  " << wtimer.timing_s() << "s" << std::endl;
 
 
   // std::cout << "run hesse" << std::endl;
@@ -94,5 +96,34 @@ void GradMinimizer() {
   // values.Print("v");
   // mu->Print("v");
   // std::cout << std::endl << std::endl;
+
+
+  // --------
+
+  std::cout << "\n === reset initial values === \n" << std::endl;
+  values = *savedValues;
+
+  // --------
+
+
+  std::cout << "trying nominal calculation AGAIN" << std::endl;
+
+  RooMinimizer m2(*nll);
+  m2.setMinimizerType("Minuit2");
+
+  m2.setStrategy(0);
+  // m2.setVerbose();
+  m2.setPrintLevel(0);
+
+  wtimer.start();
+  m2.migrad();
+  wtimer.stop();
+
+  std::cout << "  -- second nominal calculation wall clock time: " << wtimer.timing_s() << "s" << std::endl;
+
+  // m2.hesse();
+
+  // m2.minos();
+
 
 }
